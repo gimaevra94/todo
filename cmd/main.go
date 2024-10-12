3f682453eb4f6e700d511018b425d61d017beb7b
gimaevra94 <gimaevra94@ya.ru>
2024-10-12 14:28:05 +0200

    ебать я пофиксил. ебать охуеть. я молодец

--- cmd/main.go
+++ cmd/main.go
@@ -5,12 +5,26 @@ import (
 
 	todo "github.com/gimaevra94/todo"
 	handler "github.com/gimaevra94/todo/pkg/handler"
+	"github.com/gimaevra94/todo/pkg/repository"
+	"github.com/gimaevra94/todo/pkg/service"
+	"github.com/spf13/viper"
 )
 
+func initConfig() error {
+	viper.AddConfigPath("../configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
+}
+
 func main() {
-	handler := new(handler.Handler)
+	if err := initConfig(); err != nil {
+		log.Fatal(err.Error())
+	}
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handler.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf(err.Error())
 	}
 }
